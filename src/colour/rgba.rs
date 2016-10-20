e169fee86a03eb89f1a4b73f4b8a64a24cf02928
Kyle Finlay <kyle.finlay01@gmail.com>
2016-10-20 16:08:27 +1100

    Started colour module

---
+++ src/colour/rgba.rs
@@ -0,0 +1,33 @@
+/*===============================================================================================*/
+// Copyright 2016 Kyle Finlay
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/*===============================================================================================*/
+
+/*===============================================================================================*/
+/*------RGBA STRUCT------------------------------------------------------------------------------*/
+/*===============================================================================================*/
+
+/// Stores a RGBA colour value.
+pub struct RGBA {
+
+    // Public
+    /// Red channel.
+    pub r: f32,
+    /// Green channel.
+    pub g: f32,
+    /// Blue channel.
+    pub b: f32,
+    /// Alpha channel.
+    pub a: f32,
+}
